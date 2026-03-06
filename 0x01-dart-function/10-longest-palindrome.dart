c41f24ad597ebfcbaf416197ee26a99bbe6554b5
TuyenH19 <thtuyen199@gmail.com>
2026-03-06 14:46:54 +0000

    feat: create function Longest Palindrome

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,17 @@
+import '9-palindrome.dart';
+
+String longestPalindrome(String s) {
+  String longest = '';
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 1; j <= s.length; j++) {
+      String sub = s.substring(i, j);
+      if (isPalindrome(sub) && sub.length > longest.length) {
+        longest = sub;
+      }
+    }
+  }
+  if (longest.isEmpty) {
+    return 'none';
+  }
+  return longest;
+}
